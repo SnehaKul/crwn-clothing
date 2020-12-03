41d79ecb9e531c1832633e47d65be0c345022d3d
SNEHA KULKARNI <snehankis@gmail.com>
2020-12-03 17:42:13 -0600

    Added signin page,form input page and custom button

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import HomePage from './pages/homepage/homepage-component';
 import ShopPage from './pages/shop/shop-component';
 import Header from './components/header/header-component';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up-component';
 
 class App extends React.Component {
   render() {
@@ -13,6 +14,7 @@ class App extends React.Component {
         <switch>
           <Route exact path='/' component={HomePage}/>
           <Route path='/shop' component={ShopPage}/>
+          <Route path='/signin' component={SignInAndSignUpPage}/>
         </switch>
       </div>
     );
